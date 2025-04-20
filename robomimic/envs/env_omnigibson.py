19586eaffe65f1779f56368ecdac500f105440e7
Arpitrf <arpitbahety39@gmail.com>
2025-04-19 20:41:49 -0500

    added use_base_pose_hack

--- robomimic/envs/env_omnigibson.py
+++ robomimic/envs/env_omnigibson.py
@@ -340,6 +340,7 @@ class EnvOmniGibson(EB.EnvBase):
             enable_head_tracking=self.enable_head_tracking,
             curobo_batch_size=10,
             curobo_use_cuda_graph=not self.enable_head_tracking,
+            use_base_pose_hack=True
         )
 
         # Create CuRobo instance
