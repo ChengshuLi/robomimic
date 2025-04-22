7783234af83a4ba648dfc9095fc27e92a81d7cac
Chengshu Li <chengshu@stanford.edu>
2025-04-21 18:29:01 -0700

    turn off head tracking, reduce batch size for warmup

--- robomimic/envs/env_omnigibson.py
+++ robomimic/envs/env_omnigibson.py
@@ -19,6 +19,7 @@ from omnigibson import object_states
 from omnigibson.objects.primitive_object import PrimitiveObject
 from omnigibson.action_primitives.starter_semantic_action_primitives import StarterSemanticActionPrimitives
 from omnigibson.objects.dataset_object import DatasetObject
+from omnigibson.action_primitives.curobo import CuRoboEmbodimentSelection
 
 from mimicgen.train_scripts.train_prep_data import compute_point_cloud_from_rgbd
 from scipy.spatial.transform import Rotation as R
@@ -332,13 +333,13 @@ class EnvOmniGibson(EB.EnvBase):
                                       self.eef_current_marker_right, self.eef_goal_marker_right], [self.env.scene] * 4)
             og.sim.step()
 
-        self.enable_head_tracking = True
+        self.enable_head_tracking = False
         # Head tracking with soft visibility constraint requires use_cuda_graph=False
         self.primitive = StarterSemanticActionPrimitives(
             self.env,
             self.env.robots[0],
             enable_head_tracking=self.enable_head_tracking,
-            curobo_batch_size=10,
+            curobo_batch_size=4,
             curobo_use_cuda_graph=not self.enable_head_tracking,
         )
 
